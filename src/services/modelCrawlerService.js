c2f20c66a65836c238c9d02a857b7df27a3f718b
Runarry <sleep.rotten.banana@hotmail.com>
2025-05-05 12:15:23 +0800

    ♻️ refactor(modelCrawlerService): 移除未使用的构造函数及方法
    - 删除构造函数及未实现的爬取相关方法
    - 简化类结构，待后续功能扩展

--- src/services/modelCrawlerService.js
+++ src/services/modelCrawlerService.js
@@ -1,27 +1,8 @@
 const getCivitaiModelInfoWithTagsAndVersions = require("../utils/civitai-model-info-crawler")
 
 class ModelCrawlerService{
-    #sourceConfig
+
     
-
-    constructor(sourceConfig){
-        if(!sourceConfig){
-            throw new Error('ModelCrawlerService required  a SourceConfig.')
-        }
-        this.#sourceConfig = sourceConfig;
-    }
-
-    async startCrawling(){
-        // todo
-    }
-    stopCrawling(){
-        // todo
-    }
-    getCrawlStatus(){
-        // todo
-    }
-    
-
 
 }
 
